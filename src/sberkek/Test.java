9d1e02d047e6f6b7d98a54f69218ff076911d955
Abra <a@abra.me>
2018-04-27 22:52:45 +0400

    basic scaffolding

---
+++ src/sberkek/Test.java
@@ -0,0 +1,10 @@
+package sberkek;
+
+import org.pmw.tinylog.Logger;
+
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("hi");
+    Logger.info("priv");
+  }
+}
